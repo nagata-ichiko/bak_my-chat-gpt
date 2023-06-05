32e27098e0c73197cce5a43e9bac1c463e2b33c7
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-06-05 20:55:03 +0900

    Revert "最新に更新"
    This reverts commit e6deb2e3c367caa77c23b4e4a892d3b1420e727d.

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,36 +1,55 @@
-import { Message, OpenAIModel } from "@/types";
-import { OpenAIStream } from "@/utils";
+import { ChatBody, Message } from '@/types/chat';
+import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
+import { OpenAIStream } from '@/utils/server';
+import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
+import { init, Tiktoken } from '@dqbd/tiktoken/lite/init';
+// @ts-expect-error
+import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
 
 export const config = {
-  runtime: "edge"
+  runtime: 'edge',
 };
 
 const handler = async (req: Request): Promise<Response> => {
   try {
-    const { model, messages } = (await req.json()) as {
-      model: OpenAIModel;
-      messages: Message[];
-    };
+    const { model, messages, key, prompt } = (await req.json()) as ChatBody;
 
-    const charLimit = 12000;
-    let charCount = 0;
-    let messagesToSend = [];
+    await init((imports) => WebAssembly.instantiate(wasm, imports));
+    const encoding = new Tiktoken(
+      tiktokenModel.bpe_ranks,
+      tiktokenModel.special_tokens,
+      tiktokenModel.pat_str,
+    );
 
-    for (let i = 0; i < messages.length; i++) {
-      const message = messages[i];
-      if (charCount + message.content.length > charLimit) {
-        break;
-      }
-      charCount += message.content.length;
-      messagesToSend.push(message);
+    let promptToSend = prompt;
+    if (!promptToSend) {
+      promptToSend = DEFAULT_SYSTEM_PROMPT;
     }
 
-    const stream = await OpenAIStream(model, messagesToSend);
+    const prompt_tokens = encoding.encode(promptToSend);
+
+    let tokenCount = prompt_tokens.length;
+    let messagesToSend: Message[] = [];
+
+    for (let i = messages.length - 1; i >= 0; i--) {
+      const message = messages[i];
+      const tokens = encoding.encode(message.content);
+
+      if (tokenCount + tokens.length > model.tokenLimit) {
+        break;
+      }
+      tokenCount += tokens.length;
+      messagesToSend = [message, ...messagesToSend];
+    }
+
+    encoding.free();
+
+    const stream = await OpenAIStream(model, promptToSend, key, messagesToSend);
 
     return new Response(stream);
   } catch (error) {
     console.error(error);
-    return new Response("Error", { status: 500 });
+    return new Response('Error', { status: 500 });
   }
 };
 
